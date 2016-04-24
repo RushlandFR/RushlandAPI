13b70cb8152cf19912a39131462c5b371d5c2e4f
Aquazus <contact@aquazus.fr>
2016-04-24 13:01:22 +0200

    Nettoyage du code n°8

--- src/main/java/fr/rushland/api/utils/CodeUtils.java
+++ src/main/java/fr/rushland/api/utils/CodeUtils.java
@@ -2,12 +2,9 @@ package fr.rushland.api.utils;
 
 import org.bukkit.Bukkit;
 
-/**
- * Created by Zaral on 13/04/2016.
- */
 public class CodeUtils {
+	
     public static void logToConsole(String message) {
         Bukkit.getLogger().info("[RushlandAPI] " + message);
     }
-
 }
