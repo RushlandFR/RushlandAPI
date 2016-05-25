15e48df430087261acde0b8c3cb476613efedfaa
Aquazus <contact@aquazus.fr>
2016-05-25 23:16:11 +0200

    ???

--- src/main/java/fr/rushland/api/redis/Subscriber.java
+++ src/main/java/fr/rushland/api/redis/Subscriber.java
@@ -1,20 +1,29 @@
 package fr.rushland.api.redis;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import fr.rushland.api.BukkitInjector;
 import redis.clients.jedis.JedisPubSub;
 
-/**
- * Created by Zaral on 10/04/2016.
- */
 public class Subscriber extends JedisPubSub {
-
-
 
     @Override
     public void onMessage(String channel, String message) {
-        //Reçois les messages.
-        if (channel.equals(RedisDataSender.channelSub)) {
-
-
+        if (!channel.equals(RedisDataSender.channelSub)) {
+            return;
+        }
+        String[] packet = message.split("#");
+        if (!packet[0].equals(RedisDataSender.serverId)) {
+            return;
+        }
+        if (packet[1].equals("shutdown")) {
+            System.out.println("[Subscriber] Received shutdown packet");
+            for (Player players : Bukkit.getOnlinePlayers()) {
+                players.sendMessage("Votre partie a été arrêtée pour des raisons techniques.");
+                BukkitInjector.getApi().runBungeePlayerCommand(players, "hub");
+            }
+            Bukkit.getServer().shutdown();
         }
     }
 }
