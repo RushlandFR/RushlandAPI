6703316eebc0494f6a1d2ea3a77bdca4848f5744
Aquazus <contact@aquazus.fr>
2016-05-28 10:22:39 +0200

    Oubli

--- src/main/java/fr/rushland/api/commands/CommandManager.java
+++ src/main/java/fr/rushland/api/commands/CommandManager.java
@@ -5,20 +5,20 @@ import fr.rushland.api.RushlandAPI;
 
 public class CommandManager {
 
-	private RushlandAPI api;
-	private BukkitInjector rushland;
-	
-	public CommandManager(RushlandAPI api, BukkitInjector rushland) {
-		this.api = api;
-		this.rushland = rushland;
-	}
-	
-	public void load() {
-        this.rushland.getCommand("money").setExecutor(new MoneyCommand(this.api));
+    private RushlandAPI api;
+    private BukkitInjector rushland;
+
+    public CommandManager(RushlandAPI api, BukkitInjector rushland) {
+        this.api = api;
+        this.rushland = rushland;
+    }
+
+    public void load() {
+        //this.rushland.getCommand("money").setExecutor(new MoneyCommand(this.api));
         this.rushland.getCommand("grade").setExecutor(new RankCommand(this.api));
         this.rushland.getCommand("spec").setExecutor(new SpecCommand(this.api));
         this.rushland.getCommand("tp").setExecutor(new TpCommand(this.api));
         this.rushland.getCommand("leave").setExecutor(new LeaveCommand(this.api));
         this.rushland.getCommand("aacautoreport").setExecutor(new ReportCommand(this.api));
-	}
+    }
 }
