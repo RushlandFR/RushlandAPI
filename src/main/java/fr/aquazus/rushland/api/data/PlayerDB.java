1b3878c38d12870967371c3c55403924e2a4aaf2
Aquazus <contact@aquazus.fr>
2016-08-12 04:03:19 +0200

    #SoonDesGrosTrucs

--- src/main/java/fr/aquazus/rushland/api/data/PlayerDB.java
+++ src/main/java/fr/aquazus/rushland/api/data/PlayerDB.java
@@ -173,6 +173,19 @@ public class PlayerDB {
             e.printStackTrace();
         }
     }
+    
+    public void set24hGold(UUID uuid) {
+        try {
+            PreparedStatement pst;
+            pst = this.api.getDataManager().getConnection().prepareStatement("UPDATE PlayerInfo SET playerKarma = ?, expire = DATE_ADD(NOW(), INTERVAL 24 HOUR) WHERE uuid = ?");
+            pst.setString(1, "or");
+            pst.setString(2, uuid.toString());
+            pst.executeUpdate();
+            pst.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 
     public void addMonth(UUID uuid) {
         try {
